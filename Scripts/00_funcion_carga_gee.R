3698faa49cfbdf26aac4d172dcb03d1d71182bbb
Carlos M Guío B <cm.guiob@gmail.com>
2025-06-10 11:58:53 -0500

    renamed files

--- Scripts/00_funcion_carga_gee.R
+++
@@ -1,9 +0,0 @@
-
-# ==== 1. Instalación y carga de librerías necesarias ====
-if (!requireNamespace("rgee", quietly = TRUE)) install.packages("rgee")
-library(rgee)
-
-ee_install_set_pyenv(py_env = "YOUR_ENV")
-ee_install_upgrade()
-
-ee_Initialize(drive = TRUE) # Asume autenticación correcta y acceso a Google Driv
